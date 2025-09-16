83bdad56cc753a66bd9b7679a2b3125126f262e7
auggiemarignier <auggie.marignier@earth.ox.ac.uk>
2025-09-16 15:28:22 +0100

    feat: better import structure

--- src/cebmf_torch/priors/__init__.py
+++ src/cebmf_torch/priors/__init__.py
@@ -1,3 +1,17 @@
+"""Prior registry and prior classes for EBMF."""
+
+# Submodules for different prior types
+from . import base, learned, mixture, point
+
+# Main registry for prior lookups
 from .registry import PRIOR_REGISTRY
 
-__all__ = ["PRIOR_REGISTRY"]
+__all__ = [
+    # Main registry
+    "PRIOR_REGISTRY",
+    # Prior modules
+    "base",
+    "learned",
+    "mixture",
+    "point",
+]
