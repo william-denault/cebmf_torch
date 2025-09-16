6f276e5c83f7b2e98e53137f614805b9600b4840
GitHub Action <action@github.com>
2025-09-16 14:31:40 +0000

    Auto-format Python files with Ruff
    Applied by GitHub Action

--- src/cebmf_torch/__init__.py
+++ src/cebmf_torch/__init__.py
@@ -1,13 +1,12 @@
 # Main classes
-from .torch_main import CovariateParams, ModelParams, NoiseParams, cEBMF
+# Submodules for advanced usage
+from . import cebnm, ebnm, priors, utils
 
 # Core EBNM functions - commonly used building blocks
 from .ebnm.ash import ash
 from .ebnm.point_exp import ebnm_point_exp
 from .ebnm.point_laplace import ebnm_point_laplace
-
-# Submodules for advanced usage
-from . import cebnm, ebnm, priors, utils
+from .torch_main import CovariateParams, ModelParams, NoiseParams, cEBMF
 
 __all__ = [
     # Main classes
