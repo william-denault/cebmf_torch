8f9c5f71a97a406cab6c4e8140d114e86e7b923b
DeaglanBartlett <47668431+DeaglanBartlett@users.noreply.github.com>
2025-09-17 10:08:50 +0100

    Remove unnecessary api files and fix import of spiked-emdn

--- src/cebmf_torch/cebnm/__init__.py
+++ src/cebmf_torch/cebnm/__init__.py
@@ -5,12 +5,12 @@ from .cash_solver import cash_posterior_means
 from .cov_gb_prior import cgb_posterior_means
 from .cov_sharp_gb_prior import sharp_cgb_posterior_means
 from .emdn import emdn_posterior_means
-from .spiked_emdn import spiked_mdn_posterior_means
+from .spiked_emdn import spiked_emdn_posterior_means
 
 __all__ = [
     "cash_posterior_means",
     "cgb_posterior_means",
     "emdn_posterior_means",
     "sharp_cgb_posterior_means",
-    "spiked_mdn_posterior_means",
+    "spiked_emdn_posterior_means",
 ]
